6819697415d9ac3d9a470b23db12b89bac8007c1
Gaurav Mandana <53940031+gm088@users.noreply.github.com>
2023-07-14 18:36:41 +0200

    Add files via upload

---
+++ atropos/comprehensive_region_generator.R
@@ -0,0 +1,75 @@
+source("/hpcnfs/data/GN2/gmandana/bin/4.1.0/home/ieo5559/ENHANCEDCLIP/feat_extr_kmer_functions.R")
+source("/hpcnfs/data/GN2/gmandana/bin/4.1.0/home/ieo5559/insp/functions_2.R")
+source("/hpcnfs/data/GN2/gmandana/bin/4.1.0/home/ieo5559/Shannahan/functions.R")
+options(meme_bin = "/hpcnfs/home/ieo5559/meme/bin")
+genome = BSgenome.Hsapiens.UCSC.hg38.masked
+#this is for the coding gene annotation, remember to change the chromosome names
+txdb = loadDb("/hpcnfs/data/GN2/gmandana/bin/4.1.0/home/ieo5559/insp/txdb.sqlite")
+seqlevels(txdb) = paste0("chr", seqlevels(txdb))
+alltx = transcripts(txdb, use.names = T, columns=columns(txdb))
+coding = alltx[alltx$TXTYPE == "protein_coding"]
+proseq = import("TSS_analysis/denovo_sorted.bed")
+mysiW = customimportbed6("TSS_analysis/mysiW_fdr0.0001_gt1kb.bed")
+clipper_peaks = customimportbed6("TSS_analysis/clipper_new_rpkm_noucscrepeats_nosmallRNA.bed")
+new_siw = customimportbed6("/hpcnfs/data/GN2/gmandana/bin/4.1.0/home/ieo5559/ENHANCEDCLIP/new_siW/all_siW_new.bed")
+RNA_ttome = customimportbed6("/hpcnfs/data/GN2/gmandana/bin/4.1.0/home/ieo5559/Shannahan/ALLTSS_newttome_1.bed")
+Pol_ttome = customimportbed6("/hpcnfs/data/GN2/gmandana/bin/4.1.0/home/ieo5559/Shannahan/denovo_promprox.bed")
+colnames(mcols(RNA_ttome)) = c("txname", "rpkm", "basemeans", "log2FC", "pval")
+colnames(mcols(Pol_ttome)) = c("txname", "Pol_density")
+NDR = reduce(customimportbed6("/hpcnfs/data/GN2/gmandana/annotation/ATAC_HCT116_peaks.bed"), 
+             min.gapwidth = 500)
+NDR$id = paste0("NDR_", 1:length(NDR))
+
+### first, generate all system TUs; non redundant
+augmented = ttome_augment(RNA_ttome = RNA_ttome, 
+                               Pol_ttome = Pol_ttome, 
+                               NDR = NDR, 
+                               ext_for_olap = 500)
+
+augmented$NDR = sapply(str_split(names(augmented), "\\."), function(x){x[1]})
+names(augmented) = NULL
+#write.table(as.data.frame(augmented)[,c(1,2,3,6,7,5,8,9,10,11,12)],
+#            sep = "\t",
+#            quote = F,
+#            row.names = F,
+#            col.names = F,
+#            file = "augmented_NDR500bpmerged.bed")
+
+systemTUs = augmented
+systemTUs = readRDS("augmented_14Jul.RDS")
+
+##### now construct 
+# first, overlap with pcg to get the protein coding TUs
+systemTUs$iscoding = countOverlaps(systemTUs, coding) > 0
+
+####first, bidirectional (pcg-pcg)
+# same NDR, both coding
+bidir = makeGRangesFromDataFrame(as.data.frame(systemTUs) %>% 
+                           dplyr::filter(iscoding == T) %>% 
+                           group_by(NDR) %>%  ###operate by NDR
+                           dplyr::filter(n() > 1) %>%   ###must be > 1 tx per NDR
+                           dplyr::filter(length(unique(strand)) > 1),   ##must be diff strand
+                         keep.extra.columns = T)
+
+####second, divergent
+# same NDR, must have both coding and noncoding
+div = makeGRangesFromDataFrame(as.data.frame(systemTUs) %>% 
+                                   group_by(NDR) %>%  ###operate by NDR
+                                   dplyr::filter(n() > 1) %>%   ###must be > 1 tx per NDR
+                                   dplyr::filter(length(unique(strand)) > 1) %>%   ##must be diff strand
+                                   dplyr::filter(length(unique(iscoding)) > 1),   
+                                 keep.extra.columns = T)
+
+
+####third, unidirectional - don't trust this, it suffers from thresholding
+# only one tx for a given NDR
+
+
+##final, enhancer
+
+
+
+
+
+
+
---
+++ atropos/functions.R
@@ -0,0 +1,272 @@
+library(fitdistrplus)
+#library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+
+
+function_to_match_expr = function(fg, bg, grain = 0.1, ssize = 2000){
+  
+  print("make sure fg rpkm in col 'V7' and bg rpkm in col V5")
+  pc = 0.001
+  
+  #we will fit the rpkm of the foreground seqs to a lognormal dist
+  #dis$estimate[[1]] is the mean and [[2]] is the sd
+  dis = fitdist(fg$V7+pc, distr = "lnorm", method = "mme")
+  lmean = dis$estimate[[1]]
+  lsd = dis$estimate[[2]]
+  #pseudocount
+  #construct probability vector for sampling
+  #use the desnisty function to estimate the probability of each rpkm value
+  #each rpkm value in a window of 'grain'
+  
+  probs = plnorm(bg_coding$V5+pc+grain, meanlog = lmean, sdlog = lsd) - 
+    plnorm(bg_coding$V5+pc, meanlog = lmean, sdlog = lsd)
+  
+  #hist(bg_coding[sample(1:length(bg_coding), size = 2000, replace = F, prob = probs)]$V5+pc, 
+  #     breaks = 100,
+  #     col=rgb(1,0,0,1/4))
+  #hist(fg$V7+pc, breaks = 100, add = T, col=rgb(0,0,1,1/4))
+  #hist(bg_coding$V5+pc, breaks = 10000, add = T, col=rgb(0,1,0,1/4))
+  
+  reduced_bg = bg_coding[sample(1:length(bg_coding), size = ssize, replace = F, prob = probs)]
+  
+  return(reduced_bg)
+}
+
+function_to_match_CG = function(fg, bg, grain = 0.1, ssize = 2000){
+  
+  fgseqs = getSeq(BSgenome.Hsapiens.UCSC.hg38.masked, fg)
+  bgseqs = getSeq(BSgenome.Hsapiens.UCSC.hg38.masked, bg)
+  #we will fit the rpkm of the foreground seqs to a lognormal dist
+  #dis$estimate[[1]] is the mean and [[2]] is the sd
+  fg_GC_frac = letterFrequency(fgseqs, letters = c("AT", "GC"), as.prob = T)[,2]
+  bg_GC_frac = letterFrequency(bgseqs, letters = c("AT", "GC"), as.prob = T)[,2]
+  
+  dis = fitdist(fg_GC_frac, distr = "lnorm", method = "mme")
+  lmean = dis$estimate[[1]]
+  lsd = dis$estimate[[2]]
+  #pseudocount
+  pc = 0.001
+  #construct probability vector for sampling
+  #use the desnisty function to estimate the probability of each rpkm value
+  #each rpkm value in a window of 'grain'
+  probs = plnorm(bg_GC_frac+pc+grain, meanlog = lmean, sdlog = lsd) - 
+    plnorm(bg_GC_frac+pc, meanlog = lmean, sdlog = lsd)
+  
+  reduced_bg = bg[sample(1:length(bg), size = ssize, replace = F, prob = probs)]
+
+  return(reduced_bg)
+}
+
+savePlots = function(filename, fg, bg1, bg_expr, bg_GC){
+  
+  fgseqs = getSeq(BSgenome.Hsapiens.UCSC.hg38.masked, fg)
+  bgseqs = getSeq(BSgenome.Hsapiens.UCSC.hg38.masked, bg1)
+  bg_GC_seqs = getSeq(BSgenome.Hsapiens.UCSC.hg38.masked, bg_GC)
+  #we will fit the rpkm of the foreground seqs to a lognormal dist
+  #dis$estimate[[1]] is the mean and [[2]] is the sd
+  fg_GC_frac = letterFrequency(fgseqs, letters = c("AT", "GC"), as.prob = T)[,2]
+  bg_GC_frac = letterFrequency(bgseqs, letters = c("AT", "GC"), as.prob = T)[,2]
+  bg_GC_GC_frac = letterFrequency(bg_GC_seqs, letters = c("AT", "GC"), as.prob = T)[,2]
+  
+  pdf(filename)
+  #remember rpkm is V7 in fg and V5 in bg
+  pc = 0.01
+  ##plot of expression of two groups
+  par(mfrow=c(2,1), mar=c(2,2,2,2))
+  hist(log(bg1$V5 + pc), breaks = 100, xlab = "rpkm", main = "expression of regions")
+  hist(log(fg$V7 + pc), breaks = 100, add = T, col = rgb(1,0,0,1/2))
+  legend("topleft", c(paste("bg", ", n=", length(bg1), sep = "") , 
+                       paste("fg", ", n=", length(fg), sep = "")), col=c("grey", "red"), lwd=4)  
+  hist(log(bg_expr$V5 + pc), breaks = 100, xlab = "rpkm",
+       main = "expression of regions, post-filtering", col = rgb(0,0,1,1/2))
+  hist(log(fg$V7 + pc), breaks = 100, add = T, col = rgb(1,0,0,1/2))
+  legend("topleft", c(paste("bg_expr_control", ", n=", length(bg_expr), sep = "") , 
+                       paste("fg", ", n=", length(fg), sep = "")), col=c("blue", "red"), lwd=4)
+
+  bg_expr_GC_frac = letterFrequency(getSeq(BSgenome.Hsapiens.UCSC.hg38.masked, bg_expr), 
+                                    letters = c("AT", "GC"), as.prob = T)[,2]
+  
+  #plot of GC of two groups
+  par(mfrow=c(2,1), mar=c(2,2,2,2))
+  hist(bg_expr_GC_frac, breaks = 100, xlim = c(0,1), xlab = "GC content", 
+       main = "GC content of regions")
+  hist(fg_GC_frac, breaks = 100, add = T, col = rgb(1,0,0,1/2))
+  legend("topleft", c(paste("bg_expr_control", ", n=", length(bg_expr), sep = "") , 
+                       paste("fg", ", n=", length(fg), sep = "")), col=c("grey", "red"), lwd=4)  
+  
+  hist(bg_GC_GC_frac, breaks = 100, xlim = c(0,1), xlab = "GC content", 
+       main = "GC content of regions, post-filtering", col = rgb(0,0,1,1/2))
+  hist(fg_GC_frac, breaks = 100, add = T, col = rgb(1,0,0,1/2))
+  legend("topleft", c(paste("bg_expr_match_GC", ", n=", length(bg_GC_GC_frac), sep = "") , 
+                       paste("fg", ", n=", length(fg), sep = "")), col=c("blue", "red"), lwd=4)  
+  
+  #end
+  dev.off()
+}
+
+ttome_augment = function(RNA_ttome, Pol_ttome, NDR, ext_for_olap = 500){
+  
+  print(paste0("considering the first this many bp: ", ext_for_olap))
+  start.time <- Sys.time()
+  RNA5p = promoters(RNA_ttome, upstream = 0, downstream = ext_for_olap)
+  Pol5p = promoters(Pol_ttome, upstream = 0, downstream = ext_for_olap)
+  pooled = c(RNA5p, Pol5p)
+  
+  test = mergeByOverlaps(NDR, pooled)
+
+  #reset the TUs to their original length
+  pooled_unadj = c(RNA_ttome, Pol_ttome)
+  names(pooled_unadj) = pooled_unadj$txname
+  test$pooled = pooled_unadj[test$pooled$txname]
+  
+  #one sapply to index into the GRanges for each NDR
+  #second sapply to call parse_initiators
+  NDR_TUs = sapply(sapply(
+    unique(test$id), function(x){test[test$id == x,]$pooled}
+    ), parse_initiators)
+  
+  end.time <- Sys.time()
+  time.taken <- end.time - start.time
+  print(time.taken)
+  
+  return(unlist(GRangesList(NDR_TUs)))
+  
+}
+
+parse_initiators = function(ranges){
+  
+  #first, split by strand
+  r_plus = ranges[strand(ranges) == "+"]
+  r_minus = ranges[strand(ranges) == "-"]
+  
+  #if one or zero
+  if(length(r_plus) < 2){
+    r_plus_final = r_plus
+  }else{
+    ##get self-overlap
+    solap = as.table(findOverlaps(r_plus))
+    if(sum(solap == 1) > 0){ # at least one singleton
+      
+      if(all(as.table(findOverlaps(r_plus)) == 1)){    # non-overlapping singetons
+        
+        sorted = sort(r_plus)
+        #give them the same txname... use txname of 'last tx'
+        txname_grp = sorted[length(sorted)]$txname
+        
+        #assign alt to the first one
+        alternate = sorted[1]; alternate$alt = T
+        r_plus_final = c(alternate, sorted[2:length(sorted)])
+        r_plus_final$txname = txname_grp
+        
+      }else{
+        main_TSS = reduce(r_plus[solap > 1])
+        #mcols will be lost so need to reassign
+        mcols(main_TSS) = as.data.frame(r_plus[solap > 1]) %>% 
+          summarise(txname = paste(txname, collapse=';'), 
+                    rpkm = mean(rpkm, na.rm=TRUE), 
+                    basemeans = mean(basemeans, na.rm=TRUE), 
+                    log2FC = mean(log2FC, na.rm=TRUE), 
+                    pval = mean(pval, na.rm=TRUE), 
+                    Pol_density = mean(Pol_density, na.rm=TRUE))
+        
+        other_TSS = r_plus[solap == 1]
+        #tag it - if it's from the same NDR and same strand, I consider it same tx
+
+        sorted = sort(c(other_TSS, main_TSS))
+        txname_grp = sorted[length(sorted)]$txname
+        ##the first one will be tagged as 'alt'
+        alternate = sorted[1]; alternate$alt = T
+        r_plus_final = c(alternate, sorted[2:length(sorted)])
+        r_plus_final$txname = txname_grp
+        
+      }
+  
+    }else{  ##if all overlapping
+      
+      r_plus_final = reduce(r_plus)
+      
+      mcols(r_plus_final) = as.data.frame(r_plus) %>% 
+        summarise(txname = paste(txname, collapse=';'), 
+                  rpkm = mean(rpkm, na.rm=TRUE), 
+                  basemeans = mean(basemeans, na.rm=TRUE), 
+                  log2FC = mean(log2FC, na.rm=TRUE), 
+                  pval = mean(pval, na.rm=TRUE), 
+                  Pol_density = mean(Pol_density, na.rm=TRUE))
+    }
+  }
+  
+  #now minus strand... this should be a subroutine
+  if(length(r_minus) < 2){
+    r_minus_final = r_minus
+  }else{
+    ##get self-overlap
+    solap = as.table(findOverlaps(r_minus))
+    if(sum(solap == 1) > 0){ # at least one singleton
+      
+      if(all(as.table(findOverlaps(r_minus)) == 1)){    # non-overlapping singetons
+        
+        sorted = rev(sort(r_minus))
+        #give them the same txname... use txname of 'last tx'
+        txname_grp = sorted[length(sorted)]$txname
+        
+        #assign alt to the first one
+        alternate = sorted[1]; alternate$alt = T
+        r_minus_final = c(alternate, sorted[2:length(sorted)])
+        r_minus_final$txname = txname_grp
+        
+      }else{
+        main_TSS = reduce(r_minus[solap > 1])
+        #mcols will be lost so need to reassign
+        mcols(main_TSS) = as.data.frame(r_minus[solap > 1]) %>% 
+          summarise(txname = paste(txname, collapse=';'), 
+                    rpkm = mean(rpkm, na.rm=TRUE), 
+                    basemeans = mean(basemeans, na.rm=TRUE), 
+                    log2FC = mean(log2FC, na.rm=TRUE), 
+                    pval = mean(pval, na.rm=TRUE), 
+                    Pol_density = mean(Pol_density, na.rm=TRUE))
+        
+        other_TSS = r_minus[solap == 1]
+        #tag it - if it's from the same NDR and same strand, I consider it same tx
+        
+        sorted = rev(sort(c(other_TSS, main_TSS)))
+        txname_grp = sorted[length(sorted)]$txname
+        ##the first one will be tagged as 'alt'
+        alternate = sorted[1]; alternate$alt = T
+        r_minus_final = c(alternate, sorted[2:length(sorted)])
+        r_minus_final$txname = txname_grp
+      }
+      
+      }else{  ##if all overlapping
+      
+      r_minus_final = reduce(r_minus)
+      
+      mcols(r_minus_final) = as.data.frame(r_minus) %>% 
+        summarise(txname = paste(txname, collapse=';'), 
+                  rpkm = mean(rpkm, na.rm=TRUE), 
+                  basemeans = mean(basemeans, na.rm=TRUE), 
+                  log2FC = mean(log2FC, na.rm=TRUE), 
+                  pval = mean(pval, na.rm=TRUE), 
+                  Pol_density = mean(Pol_density, na.rm=TRUE))
+    }
+  }
+  
+  return(c(r_plus_final, r_minus_final))
+  
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
